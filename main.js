a633d7153dca2a01a3feb45d356307281005c30d
lucask95 <lucaskeller1995@gmail.com>
2017-08-18 10:49:25 -0700

    Implemented findPath()

--- main.js
+++ main.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
 // -----------------------------------------------------//
 
 class grid {
+    // class variables:
+    // grid (2d array of tiles), dirtyTiles (array of tiles), rows, cols
     constructor(rows, cols) {
         this.grid = [];
         this.rows = rows;
         this.cols = cols;
-        this.populateGrid(rows, cols);
         this.dirtyTiles = [];
+        this.populateGrid(rows, cols);
     }
 
     populateGrid(rows, cols) {
@@ -31,6 +33,8 @@ class grid {
 }
 
 class tile {
+    // class variables:
+    // i, j, dirty (boolean)
     constructor(p, i, j) {
         // p = probability that cell is dirty
         if (Math.random() <= p)
@@ -50,10 +54,16 @@ class bot {
 }
 
 // -----------------------------------------------------//
-// Algorithm
+// Global Variables
 // -----------------------------------------------------//
 
-// TODO: Implement greedy algorithm: nearest neighbor
+var myGrid;
+var myBot;
+var inProgress = false;
+
+// -----------------------------------------------------//
+// Algorithm
+// -----------------------------------------------------//
 
 // findPath() takes in a grid and returns
 // the list of tiles to clean, in order.
@@ -62,22 +72,53 @@ class bot {
 //     [1,3] // go to tile 1,3 second
 //     [3,4] // go to tile 3,4 third
 // ];
-function findPath(gridIn) {
+// uses the greedy algorithm of finding nearest dirty tile
+function findPath() {
+    var dirtyTiles = myGrid.dirtyTiles;
+    var startingLength = dirtyTiles.length;
     var tilePath = [];
-    var dirtyTiles = [];
-    for (var i = 0; i < gridIn.dirtyTiles.length; i++) {
-        // for each dirty tile, find the nearest dirty tile
+
+    // for each dirty tile, find the nearest dirty tile via manhattan length
+    for (var i = 0; i < startingLength; i++) {
+        var minDistance = 100;
+        var closestTile;
+        var closestTileIndex;
+        var tempDistance = 0;
+
+        // check all other tiles that are not yet in path
+        for (var j = 0; j < dirtyTiles.length; j++) {
+            if (i == 0) {
+                // find dirty tile nearest to bot
+                tempDistance  = Math.abs(dirtyTiles[j].i - myBot.i);
+                tempDistance += Math.abs(dirtyTiles[j].j - myBot.j);
+            }
+            else {
+                // find nearest dirty tile to current tile
+                tempDistance  = Math.abs(dirtyTiles[j].i - tilePath[i-1].i);
+                tempDistance += Math.abs(dirtyTiles[j].j - tilePath[i-1].j);
+            }
+
+            // if tile being checked is closer than current closest tile
+            // then replace closest tile
+            if (tempDistance < minDistance) {
+                minDistance = tempDistance;
+                closestTile = dirtyTiles[j];
+                closestTileIndex = j;
+            }
+        }
+
+        // add nearest dirty tile to path and remove it from the array of tiles
+        // that are not yet in the path
+        tilePath.push(closestTile);
+        dirtyTiles.splice(closestTileIndex, 1);
     }
+
     return tilePath;
 }
 
 // -----------------------------------------------------//
 // Initialization
 // -----------------------------------------------------//
-
-var myGrid;
-var myBot;
-var inProgress = false;
 
 function renderGrid() {
     // create classtext var here so it does not keep getting deleted and recreated
@@ -129,4 +170,5 @@ function startCleaning() {
     myGrid = new grid(rows, cols);
     renderGrid();
     renderBot();
+    findPath();
 }
